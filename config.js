3fbe678f430e6b3a4cb60ed2c58254c0836daaf2
Joe Ertaba <belaviyo@gmail.com>
2017-07-23 13:13:23 +0430

    supporting wget and fdm on Firefox

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 'use strict';
 
 exports.id = 'com.add0n.native_client';
-exports.version = '0.2.1';
+exports.version = '0.2.2';
 
 exports.ids = {
   chrome: [
@@ -24,5 +24,7 @@ exports.ids = {
     '{3e0ac434-26e0-4c03-b757-3078486800c3}', // Send to VLC
     '{d1646fcf-76ad-49c5-b8b2-e496e9b71189}', // Download with IDM
     '{cfd8df21-e05f-46e9-8ea1-af5e5177d492}', // Download with DAP
+    '{1fb1ffdc-b95d-451e-be52-7303adf9a0d3}', // Download with FDM
+    '{533953f8-ffb6-421c-af1a-5a02a792ab51}', // Download with Wget
   ]
 };
