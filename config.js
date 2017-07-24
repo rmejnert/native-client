9f810ceaa6098fc3adb69f354796328eca7ecde8
Joe Ertaba <belaviyo@gmail.com>
2017-07-24 14:37:30 +0430

    Supporting MPC and MPlayer on Chrome and Opera. MPC on Firefox

--- config.js
+++ config.js
@@ -2,12 +2,16 @@
 'use strict';
 
 exports.id = 'com.add0n.native_client';
-exports.version = '0.2.2';
+exports.version = '0.2.3';
 
 exports.ids = {
   chrome: [
     'hfckgfbhdacemicpjljhfbjmkiggeche', // Send to VLC (Chrome)
     'hgadfjhoalnaidfdoilgeimnolhcodbc', // Send to VLC (Opera)
+    'jdemocppkpiaocccmmdjnpclehbajogd', // Send to MPC (Chrome)
+    'ngaohleapbfjleillajbginbiaokmncm', // Send to MPC (Opera)
+    'bnjifjcdaiecofdljgempabjecjdjpnm', // Send to MPlayer (Chrome)
+    'kalnbllmkdiidgakcakkoecaahkpakmj', // Send to MPlayer (Opera)
     'nlbhcidbelfhbpglaaoljbfpfpdkpdcg', // Download with IDM (Chrome)
     'kajaikkhnmegmfnlifeklklaienhdekb', // Download with IDM (Opera)
     'jlodlegnpjplclncjkgolcmdhjmlokna', // Download with FDM (Chrome)
@@ -18,9 +22,11 @@ exports.ids = {
     'ekeecmblpnobdaijmfkcfcnofopooipg', // Download with DAP (Opera)
     'jplcfomghgpjlgglocndioeceojhoika', // Download with Wget (Chrome)
     'cmddfbnhpokddncdalabdlmdckcdmboh', // Download with Wget (Opera)
+    'ncohpappfhkpihndmkcbgkcilmlbnnai', // TEST
   ],
   firefox: [
     '{3e0ac434-26e0-4c03-b757-3078486800c3}', // Send to VLC
+    '{cee1ccee-5508-4927-a929-9a557e63bbc8}', // Send to MPC
     '{d1646fcf-76ad-49c5-b8b2-e496e9b71189}', // Download with IDM
     '{cfd8df21-e05f-46e9-8ea1-af5e5177d492}', // Download with DAP
     '{1fb1ffdc-b95d-451e-be52-7303adf9a0d3}', // Download with FDM
