b1d2c90dc453596461892fe6cf296388d02196c5
Joe Ertaba <belaviyo@gmail.com>
2017-11-24 12:03:46 +0330

    version 0.3.6

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 'use strict';
 
 exports.id = 'com.add0n.native_client';
-exports.version = '0.3.5';
+exports.version = '0.3.6';
 
 exports.ids = {
   chrome: [
@@ -30,11 +30,13 @@ exports.ids = {
   firefox: [
     '{3e0ac434-26e0-4c03-b757-3078486800c3}', // Send to VLC
     '{cee1ccee-5508-4927-a929-9a557e63bbc8}', // Send to MPC
+    '{ccad95df-add6-4d8a-aa5c-cdc384075bab}', // Send to MPV
+    '{c7636e52-8aba-4717-9652-01922ee61eb3}', // Send to MPlayer media player
     '{d1646fcf-76ad-49c5-b8b2-e496e9b71189}', // Download with IDM
     '{cfd8df21-e05f-46e9-8ea1-af5e5177d492}', // Download with DAP
     '{1fb1ffdc-b95d-451e-be52-7303adf9a0d3}', // Download with FDM
     '{533953f8-ffb6-421c-af1a-5a02a792ab51}', // Download with Wget
-    '{5510b212-951a-439c-ae73-b1ebbc68055f}', // Edit with Sublime Text
-    '{ccad95df-add6-4d8a-aa5c-cdc384075bab}', // Send to MPV
+    '{533953f8-ffb6-421c-af1a-5a02a792ab51}', // Download with JDownloader
+    '{03e07985-30b0-4ae0-8b3e-0c7519b9bdf6}', // Edit with Sublime Text
   ]
 };
